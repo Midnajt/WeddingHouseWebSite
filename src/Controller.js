d14396e42c9bd1f8bca4e1be6e01841c085b1461
Midnajt <midnajt0@gmail.com>
2022-02-20 12:50:55 +0100

    first section, with menu features done

--- src/Controller.js
+++ src/Controller.js
@@ -1,1 +1,38 @@
 console.log('elo');
+
+const hamburgerBtn = document.querySelector('#hamburger');
+
+hamburgerBtn.addEventListener('click', () => {
+    const btn = hamburgerBtn.querySelector('.hamburger')
+    const menuScreen = document.querySelector('.menu-screen')
+    btn.classList.toggle('cross-animation')
+    menuScreen.classList.toggle('show')
+})
+
+
+
+
+// class Controller {
+//     constructor(){
+//         this.init();
+//     }
+
+//     #hamburgerMenu = document.querySelector('#hamburger');
+
+//     init(){
+//         console.log('init');
+//         this.addEvents();
+//     }
+
+//     addEvents(){
+//         console.log('events');
+//         console.log(this.#hamburgerMenu);
+//         this.#hamburgerMenu.addEventListener('click', (event) => {
+//             console.log('klik');
+//             console.log(event.target);
+//         })
+//     }
+// }
+
+// const controller = new Controller();
+
