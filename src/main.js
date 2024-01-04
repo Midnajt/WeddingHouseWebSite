5a35c13ae85ecdee8a7fd6cbd3515ffe0339e1ee
Midnajt <midnajt0@gmail.com>
2024-01-04 19:47:44 +0100

    new year commercial removed

--- src/main.js
+++ src/main.js
@@ -1,53 +1,53 @@
-const hamburgerBtn = document.querySelector('#hamburger');
-const btn = hamburgerBtn.querySelector('.hamburger')
-const upBtn = document.querySelector('.upBtn')
-const galleryBtn = document.querySelector('#galleryBtn')
+const hamburgerBtn = document.querySelector("#hamburger");
+const btn = hamburgerBtn.querySelector(".hamburger");
+const upBtn = document.querySelector(".upBtn");
+const galleryBtn = document.querySelector("#galleryBtn");
 
-hamburgerBtn.addEventListener('click', () => {
-    const menuScreen = document.querySelector('.menu-screen')
-    btn.classList.toggle('cross-animation')
-    menuScreen.classList.toggle('show')
-})
+hamburgerBtn.addEventListener("click", () => {
+  const menuScreen = document.querySelector(".menu-screen");
+  btn.classList.toggle("cross-animation");
+  menuScreen.classList.toggle("show");
+});
 
-document.addEventListener('scroll', function(){
-    const heightToMenu = $('.mainMenu__con').offset().top;
+document.addEventListener("scroll", function () {
+  const heightToMenu = $(".mainMenu__con").offset().top;
 
-    if(window.scrollY >= heightToMenu){
-        btn.classList.add('bgcColor');
-        upBtn.classList.add('show');
-    } else {
-        btn.classList.remove('bgcColor');
-        upBtn.classList.remove('show');
-    }
+  if (window.scrollY >= heightToMenu) {
+    btn.classList.add("bgcColor");
+    upBtn.classList.add("show");
+  } else {
+    btn.classList.remove("bgcColor");
+    upBtn.classList.remove("show");
+  }
 });
 
 const moveScreen = (e) => {
-    if(btn.classList.contains('cross-animation')){
-        console.log('warunek spełniony');
-        hamburgerBtn.click();
-    }
-    const direction = e.target.dataset.direction
-    const chosenSection = document.querySelector(`.${direction}`)
-    $("html, body").animate({ scrollTop: $(chosenSection).offset().top -90 }, 500);
-}
+  if (btn.classList.contains("cross-animation")) {
+    console.log("warunek spełniony");
+    hamburgerBtn.click();
+  }
+  const direction = e.target.dataset.direction;
+  const chosenSection = document.querySelector(`.${direction}`);
+  $("html, body").animate({ scrollTop: $(chosenSection).offset().top - 90 }, 500);
+};
 
-$(upBtn).click(function() {
-    $("html, body").animate({ scrollTop: $('.main').offset().top }, 500);
+$(upBtn).click(function () {
+  $("html, body").animate({ scrollTop: $(".main").offset().top }, 500);
 });
 
 lightbox.option({
-    'resizeDuration': 150,
-    'wrapAround': true
-})
+  resizeDuration: 150,
+  wrapAround: true,
+});
 
-galleryBtn.addEventListener('click', () => {
-    $("html, body").animate({ scrollTop: $('.gallery__con').offset().top -90 }, 500);
-    const galleryCon = document.querySelector('.gallery__con');
-    galleryCon.classList.toggle('show');
+galleryBtn.addEventListener("click", () => {
+  $("html, body").animate({ scrollTop: $(".gallery__con").offset().top - 90 }, 500);
+  const galleryCon = document.querySelector(".gallery__con");
+  galleryCon.classList.toggle("show");
 
-    if(galleryCon.classList.contains('show')){
-        galleryBtn.innerText = 'Zwiń';
-    } else {
-        galleryBtn.innerText = 'Pokaż więcej';
-    }
-})
+  if (galleryCon.classList.contains("show")) {
+    galleryBtn.innerText = "Zwiń";
+  } else {
+    galleryBtn.innerText = "Pokaż więcej";
+  }
+});
